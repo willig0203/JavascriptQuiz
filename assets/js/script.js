3195a840bd740b9b282b939a9b3f97b5ed46dbd3
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-05-25 22:12:29 -0500

    testing buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,60 @@ var quizEl = document.querySelector("#btnstartquiz");
 
 quizEl.addEventListener("click", removeStartPage);
 
+
+
+var btnSelected1 = document.querySelector("#btnAns1");
+var btnSelected2 = document.querySelector("#btnAns2");
+var btnSelected3 = document.querySelector("#btnAns3");
+var btnSelected4 = document.querySelector("#btnAns4");
+btnSelected1.addEventListener("click", selectAnswer);
+btnSelected2.addEventListener("click", selectAnswer);
+btnSelected3.addEventListener("click", selectAnswer);
+btnSelected4.addEventListener("click", selectAnswer);
+
+function selectAnswer(event) {
+    console.log("selectAnswer" + " " + event.target);
+    console.log(object.innerHTML);
+}
+
+
+
 function removeStartPage(event) {
-    console.log(event.target);
     quizEl.parentElement.remove("mainstartquiz");
-    console.log("gone");
-
-    // start timer
-    
     startTimer();
-
+    showQuestions();
     // display for.......
 };
-
 
 function startTimer() {
     var minute = 5;
     var sec = 60;
-    setInterval(function() {
-      document.getElementById("#quiztimeleft").innerHTML = minute + " : " + sec;
-      sec--;
-      if (sec == 00) {
-        minute --;
-        sec = 60;
-        if (minute == 0) {
-          minute = 5;
+    setInterval(function () {
+        document.getElementById("#quiztimeleft").innerHTML = minute + " : " + sec;
+        sec--;
+        if (sec == 00) {
+            minute--;
+            sec = 60;
+            if (minute == 0) {
+                minute = 5;
+            }
         }
-      }
     }, 1000);
-}
+};
 
 
+
+function hidQuestions(event) {
+    const button = document.querySelector('.mainstartquestions');
+    button.classList.add('hidden');
+};
+
+function showQuestions(event) {
+    const button = document.querySelector('.mainstartquestions');
+    button.classList.remove('hidden');
+};
+
+hidQuestions();
 
 
 
