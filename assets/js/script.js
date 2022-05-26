3155fd7b3b395e80bd4f9a18cf0d8a75690cb468
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-05-25 19:47:40 -0500

    timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,39 @@
+
 var quizEl = document.querySelector("#btnstartquiz");
 
 quizEl.addEventListener("click", removeStartPage);
 
 function removeStartPage(event) {
     console.log(event.target);
-    quizEl.parentElement.remove("mainstartquiz");    
+    quizEl.parentElement.remove("mainstartquiz");
     console.log("gone");
+
+    // start timer
+    
+    startTimer();
+
+    // display for.......
 };
+
+
+function startTimer() {
+    var minute = 5;
+    var sec = 60;
+    setInterval(function() {
+      document.getElementById("#quiztimeleft").innerHTML = minute + " : " + sec;
+      sec--;
+      if (sec == 00) {
+        minute --;
+        sec = 60;
+        if (minute == 0) {
+          minute = 5;
+        }
+      }
+    }, 1000);
+}
+
+
+
+
 
 
